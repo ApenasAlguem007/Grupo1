00c523a6f9157eedd8ef4c7a99b89a6a4ef1a6c8
ApenasAlguem007 <geovanna.psilva2005@gmail.com>
2025-02-21 11:44:42 -0300

    matriz

---
+++ Proj-043/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matriz</title>
+</head>
+<body>
+
+    <h1> Matriz </h1>
+
+    <?php 
+    
+    $matriz =[
+        
+        [10, 16, 1, 5],
+        [2, 3, 4, 9],
+        [11, 6, 7, 8],
+        [12, 13, 14, 15]
+    
+        ];
+        
+        $soma = 1;
+ 
+    foreach ($matriz as $linha){
+        foreach ($linha as $elemento)
+        $soma *= $elemento;
+    }
+ 
+    echo "A soma da Matriz total é: " . $soma;
+ 
+    
+        
+
+    ?>
+
+    
+        
+
+    
+</body>
+</html>
